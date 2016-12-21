20b4550d039015e18bfe5d272cf5179f7676d91c
Anthony Tsai (DCS-TW) <anthony_tsai@trend.com.tw>
2016-12-22 02:10:17 +0800

    getting started

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from '../components/search_bar'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyCRKW-w3xjHVzuNNwYgCAiCMdfmgKDs6ko';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = ()=>{
+  return <div>Hi</div>;
+}
+
+ReactDOM.render(<App />,document.querySelector('.container'));
